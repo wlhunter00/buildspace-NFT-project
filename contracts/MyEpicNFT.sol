0d65a6f996f87b7f58a4f7b44c1ec0de5e8b2b20
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-12-20 23:11:01 -0500

    on-chain metadata

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -16,7 +16,10 @@ contract MyEpicNFT is ERC721URIStorage {
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/W0C2");
+        _setTokenURI(
+            newItemId,
+            "data:application/json;base64,ewogICAgIm5hbWUiOiAiV2lsbCdzIChPbi1DaGFpbikgV2ViMyBXZWxjb21lIiwKICAgICJkZXNjcmlwdGlvbiI6ICJXZWxjb21lIHRvIFdlYjMhIEhlcmUgaXMgeW91ciBmaXJzdCBORlQuIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStWMmxzYkNkeklGZGxZak1nVjJWc1kyOXRaVHd2ZEdWNGRENEtQQzl6ZG1jKyIKfQ"
+        );
         _tokenIds.increment();
         console.log(
             "An NFT w/ ID %s has been minted to %s",
